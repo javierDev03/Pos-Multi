eb33af917dde25d223a28130c46ea5ad8fe2318d
Javier Cervantes <javicervel250@gmail.com>
2025-10-10 15:34:31 -0600

    Remove unused user entries and roles from UserSeeder to streamline database seeding.

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -31,48 +31,7 @@ class UserSeeder extends Seeder
                 'email_verified_at' => now(),
                 'created_at'        => now(),
             ],
-            [
-                'name'              => 'Norita Financiero',
-                'email'             => 'rf_cenidet@tecnm.mx',
-                'password'          => Hash::make('citca2025xo'),
-                'email_verified_at' => now(),
-                'created_at'        => now(),
-            ],
-            [
-                'name'              => 'Ricardo Fabricio Escobar Jimenez',
-                'email'             => 'ricardo.ej@cenidet.tecnm.mx',
-                'password'          => Hash::make('citca2025fab'),
-                'email_verified_at' => now(),
-                'created_at'        => now(),
-            ],
-            [
-                'name'              => 'Carlos Daniel García Beltrán',
-                'email'             => 'carlos.gb@cenidet.tecnm.mx',
-                'password'          => Hash::make('citca2025car'),
-                'email_verified_at' => now(),
-                'created_at'        => now(),
-            ],
-            [
-                'name'              => 'Vitervo López Caballero',
-                'email'             => 'vitervolopezcaballero@gmail.com',
-                'password'          => Hash::make('Password'),
-                'email_verified_at' => now(),
-                'created_at'        => now(),
-            ],
-            [
-                'name'              => 'Vitervo López Caballero',
-                'email'             => 'veranocientifico@cenidet.tecnm.mx',
-                'password'          => Hash::make('Password'),
-                'email_verified_at' => now(),
-                'created_at'        => now(),
-            ],
-            [
-                'name'              => 'Vitervo López Caballero',
-                'email'             => 'vitervo@cenidet.edu.mx',
-                'password'          => Hash::make('Password'),
-                'email_verified_at' => now(),
-                'created_at'        => now(),
-            ],
+            
         ]);
 
         // Crear roles
@@ -82,18 +41,11 @@ class UserSeeder extends Seeder
         $postulant    = Role::create(['name' => 'Postulante', 'description' => 'Postulante']);
         $financial    = Role::create(['name' => 'Finanzas', 'description' => 'Finanzas']);
         $adminrevista = Role::create(['name' => 'Admin-Revista', 'description' => 'Admin-Revista']);
-        $conferencista= Role::create(['name' => 'Conferencista', 'description' => 'Conferencista']);
-        $tallerista   = Role::create(['name' => 'Tallerista', 'description' => 'Tallerista']);
-        $asistente    = Role::create(['name' => 'Asistente', 'description' => 'Asistente']);
+      
 
         // Asignar roles a usuarios existentes
         User::find(1)->assignRole($admin);
         User::find(2)->assignRole($admin);
-        User::find(3)->assignRole($financial);
-        User::find(4)->assignRole($adminrevista);
-        User::find(5)->assignRole($adminrevista);
-        User::find(6)->assignRole($postulant);
-        User::find(7)->assignRole($editor);
-        User::find(8)->assignRole($reviewer);
+        
     }
 }
